b9b66a1d9db6aed8b55eb7963a1eff66bddb3b1d
Jimmy <toluadejimi@gmail.com>
2024-06-02 05:25:16 +0100

    first commit

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Models\Deposit;
+use App\Models\Nowpayment;
+
+class ApiController extends Controller
+{
+    public function e_check(request $request){
+
+        $get_user =  User::where('email', $request->email)->first() ?? null;
+
+        if($get_user == null){
+
+            return response()->json([
+                'status' => false,
+                'message' => 'No user found, please check email and try again',
+            ]);
+        }
+
+
+        return response()->json([
+            'status' => true,
+            'user' => $get_user->username,
+        ]);
+
+    }
+
+
+    public function e_fund(request $request){
+
+        $get_user =  User::where('email', $request->email)->first() ?? null;
+
+        if($get_user == null){
+
+            return response()->json([
+                'status' => false,
+                'message' => 'No user found, please check email and try again',
+            ]);
+        }
+
+        User::where('email', $request->email)->increment('total_money', $request->amount) ?? null;
+
+        Nowpayment::where('invoice_id', $request->order_id)->update(['payment_status'=> "Success"]);
+
+        $amount = number_format($request->amount, 2);
+
+        return response()->json([
+            'status' => true,
+            'message' => "NGN $amount has been successfully added to your wallet",
+        ]);
+
+    }
+}
